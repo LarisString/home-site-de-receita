2da919706f1cdaf8056b4346c80c4104b0683e60
LarisString <larissastring@gmail.com>
2023-08-28 11:16:27 -0300

    :configurando o botão do nav

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const hamburguer = document.querySelector (".hamburguer");
 const nav = document.querySelector (".nav");
 
-hamburguer.addEventListener ('click', () =>nav.classList.toggle("active"));
+hamburguer.addEventListener ('click', () =>nav.classList.toggle("inactive"));

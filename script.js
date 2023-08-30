42da21e13b4c90d33fa44e9695820ed4bc0ee271
LarisString <larissastring@gmail.com>
2023-08-29 22:34:36 -0300

    :tentei arrumar o js e falhei

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-const hamburguer = document.querySelector (".hamburguer");
-const nav = document.querySelector (".nav");
 
-hamburguer.addEventListener ('click', () =>nav.classList.toggle("inactive"));
+document.getElementById("botao").disabled = true;
+
+document.getElementById("nav").addEventListener("click", () =>nav.classList.toggle("active"));
+    var lista = document.getElementById("nav-list").value;
+
+    if (document.getElementById("nav") !== null && document.getElementById("nav") !== 'lista') {
+        document.getElementById("botao").disabled = false;
+    } else {document.getElementById("botao").disabled = true;
+}
+;
